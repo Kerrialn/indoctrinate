29773535268366cc3aff46645e2e0706320732f6
kez <kerrialbeckettnewham@gmail.com>
2026-05-29 12:38:38 +0200

    udpate indoctrinate.dist.php

--- indoctrinate.dist.php
+++ indoctrinate.dist.php
@@ -1,21 +1,32 @@
 <?php
 
 use Indoctrinate\Config\IndoctrinateConfig;
+use Indoctrinate\Rule\MySQL\Integrity\Constraint\ConvertTemporalColumnsToDatetimeRuleConstraints;
+use Indoctrinate\Rule\MySQL\Integrity\Constraint\EnsurePrimaryKeyUuidRuleConstraints;
+use Indoctrinate\Rule\MySQL\Integrity\Constraint\EnsureUnifiedPrimaryKeyNameRuleConstraints;
+use Indoctrinate\Rule\MySQL\Integrity\Constraint\NormalizeTemporalValuesRuleConstraints;
+use Indoctrinate\Rule\MySQL\Integrity\ConvertTemporalColumnsToDatetimeRule;
+use Indoctrinate\Rule\MySQL\Integrity\EnsurePrimaryKeyUuidRule;
+use Indoctrinate\Rule\MySQL\Integrity\EnsureUnifiedPrimaryKeyNameRule;
+use Indoctrinate\Rule\MySQL\Integrity\NormalizeTemporalValuesRule;
+use Indoctrinate\Rule\MySQL\Normalization\Constraint\SlugifyFieldRuleConstraints;
+use Indoctrinate\Rule\MySQL\Normalization\SlugifyFieldRule;
 use Indoctrinate\Set\MySQL\DoctrineCompatibilitySet;
+use Indoctrinate\Set\MySQL\EnsureDateTimeSet;
+use Indoctrinate\Set\MySQL\EnsurePrimaryKeyUuidSet;
 
 return static function (IndoctrinateConfig $config): void {
 
-    $config->setConnectionCredentials(
-        driver: 'mysql',
-        host: '127.0.0.1',
-        port: 4406,
-        dbname: 'db_name',
-        user: 'user',
-        password: 'password',
+    $config->connection(
+        'mysql',
+        '127.0.0.1',
+        4406,
+        '',
+        '',
+        '',
     );
 
     $config->sets([
         DoctrineCompatibilitySet::class => []
     ]);
-
 };
